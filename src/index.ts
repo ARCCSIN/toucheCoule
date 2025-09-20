a6cfe7cf8704a5cd18aa008b195b3a7e6cf57280
. BLENET <blenetguilhem@gmail.com>
2025-09-20 11:49:37 +0200

    feat: ajouter un bateau de maniere random

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,41 @@ const readl = readline.createInterface({
 const size = 10;
 const grilleBateaux: string[][] = Array.from({length:size }, ()=> Array(size).fill(" ")
 );
+
+function placerBateauRandom(taille:number){
+    let placed= false;
+
+    while (!placed){
+        let ligne = Math.floor(Math.random()* size);
+        let column = Math.floor(Math.random()*size);
+        let horizontal = Math.random() < 0.5;
+
+        if (horizontal){
+            if (column + taille <= size){
+                let place = true;
+                for (let i = 0; i< taille; i++){
+                    if(grilleBateaux[ligne]![column+i] === 'B') place = false;
+                }
+            if (place){
+                for (let i = 0; i < taille; i++){
+                    grilleBateaux[ligne]![column+i] = "Bateau";
+                }
+                placed = true;
+        }
+      }
+    } else {
+      if (ligne + taille <= size) {
+        let libre = true;
+        for (let i = 0; i < taille; i++) {
+          if (grilleBateaux[ligne + i]![column] === "Bateau") libre = false;
+        }
+        if (libre) {
+          for (let i = 0; i < taille; i++) {
+            grilleBateaux[ligne + i]![column] = "Bateau";
+          }
+          placed = true;
+        }
+      }
+    }
+  }
+}
